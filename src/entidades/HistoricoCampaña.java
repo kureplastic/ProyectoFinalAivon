ef05a33ee67ba7fe1344cefdf7284d374be48bd1
Fran Gonzalez <Fran Gonzalez@FRANGE>
2020-10-31 06:22:32 -0300

    Proyecto final

--- src/entidades/HistoricoCampaña.java
+++ src/entidades/HistoricoCampaña.java
@@ -10,5 +10,60 @@ package entidades;
  * @author Fran Gonzalez
  */
 public class HistoricoCampaña {
+   
+    private int idHistoricoCampaña;
+    private Campaña campaña;
+    private Revendedor revendedor;
+    private int estrellasRC;
+
+    public HistoricoCampaña() {
+    }
+      
+    public HistoricoCampaña(int idHistoricoCampaña, Campaña campaña, Revendedor revendedor, int estrellasRC) {
+        this.idHistoricoCampaña = idHistoricoCampaña;
+        this.campaña = campaña;
+        this.revendedor = revendedor;
+        this.revendedor.setActivo = true;
+        this.estrellasRC = estrellasRC;
+    }
+
+    public HistoricoCampaña(Campaña campaña, Revendedor revendedor, int estrellasRC) {
+        this.campaña = campaña;
+        this.revendedor = revendedor;
+        this.revendedor.setActivo = true;
+        this.estrellasRC = estrellasRC;
+    }
+
+    public int getIdHistoricoCampaña() {
+        return idHistoricoCampaña;
+    }
+
+    public Campaña getCampaña() {
+        return campaña;
+    }
+
+    public void setCampaña(Campaña campaña) {
+        this.campaña = campaña;
+    }
+
+    public Revendedor getRevendedor() {
+        return revendedor;
+    }
+
+    public void setRevendedor(Revendedor revendedor) {
+        this.revendedor = revendedor;
+    }
+
+    public int getEstrellasRC() {
+        return estrellasRC;
+    }
+
+    public void setEstrellasRC(int estrellasRC) {
+        this.estrellasRC = estrellasRC;
+    }
+    
+    
+    
+    
     
 }
--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -9,6 +9,36 @@ package modelo;
  *
  * @author Fran Gonzalez
  */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class Conexion {
     
+    
+    
+    private final String base = "aivon";
+    private final String url="jdbc:mysql://localhost:3306/" + base;
+    private final String user="root";
+    private final String pass="";
+    private Connection con;
+    
+    public Connection getConnection()
+    {
+        if(con==null)
+        {
+            try
+            {
+                Class.forName("org.mariadb.jdbc.Driver");
+                con = (Connection)DriverManager.getConnection(url, user, pass);
+            }
+            catch (SQLException |ClassNotFoundException e)
+            {
+                JOptionPane.showMessageDialog(null, "Error al conectarse");
+            }
+        }
+                return con;
+    }
 }
