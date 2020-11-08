08bb07ca151e250a95dfa399dd459cfe9f309b87
Fran Gonzalez <Fran Gonzalez@FRANGE>
2020-11-08 20:33:08 -0300

    Modificacion de algunos metodos que tenian errores y adiciones de otros en distintas clases. Se agrego la info actualizada despues de la actualizacion que dieron los profes. Tambien se subio una nueva base de datos.

--- src/modelo/detallePedidoData.java
+++ src/modelo/detallePedidoData.java
@@ -5,10 +5,40 @@
  */
 package modelo;
 
+import entidades.DetallePedido;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Fran Gonzalez
  */
 public class detallePedidoData {
     
+   private Connection con;
+   
+   public detallePedidoData(Conexion conexion){
+       con = conexion.getConnection();
+   }
+   
+   public void guardarDetalle(DetallePedido detalle){
+       String sql = "INSERT INTO detallepedido (idProducto, idPedido, cantProducto, estrellasTotales)" 
+                    + "VALUES(?,?,?,?)";
+       
+       
+       try {
+           PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+           
+           
+           
+       } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error: " + ex);
+       }
+       
+   }
+   
+   
 }
