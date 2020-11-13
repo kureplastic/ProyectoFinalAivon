ebf3f87dff5b740e1bd8be1dda91d08fd863eb23
Fran Gonzalez <Fran Gonzalez@FRANGE>
2020-11-13 18:57:45 -0300

    Se puso en comentario las pruebas y que generaban conflicto con la version correcta de las clases respectivas. Se debera hacer analisis sobre eso.

--- src/aivon/Aivon.java
+++ src/aivon/Aivon.java
@@ -127,7 +127,7 @@ public class Aivon {
         */
         
         System.out.println(prData1.buscarProducto(114).getDescripcion());       //buscar producto antes de actualizar
-        prData1.actualizarProducto(prod1);                                    //actualizar producto 
+        //prData1.actualizarProducto(prod1);                                    //actualizar producto 
         System.out.println(prData1.buscarProducto(1).getDescripcion());       //buscar producto despues de actualizar
         
         
